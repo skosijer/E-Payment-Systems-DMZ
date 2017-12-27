6482645c3a3f04adbcc342f0a96fadb481c8714a
= <babic.milan999@yahoo.com>
2017-12-27 21:06:10 +0100

    dobavljanje starosnih grupa iz baze

--- src/main/java/dmz/insurance/rest/MainController.java
+++ src/main/java/dmz/insurance/rest/MainController.java
@@ -1,24 +1,26 @@
 package dmz.insurance.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@CrossOrigin
+@RequestMapping("/dmzMain")
+public class MainController {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/dobaviKategoriju")
+	@Value("${corp.url}")
+	private String corpUrl;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/dobaviStarosneGrupe")
 	public ResponseEntity<?> fillInsuranceDatabase() {
 		RestTemplate restTemplate = new RestTemplate();
-		String dcResourceUrl
-		  = "http://localhost:8082/kategorija/dobaviKategoriju";
-		ResponseEntity<String> response
-		  = restTemplate.getForEntity(dcResourceUrl + "/1", String.class);
-		
-		return new ResponseEntity<>(HttpStatus.OK);
+		ResponseEntity<?> response = restTemplate.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviStarosneGrupe", String.class);
+		return response;
 	}
 }
