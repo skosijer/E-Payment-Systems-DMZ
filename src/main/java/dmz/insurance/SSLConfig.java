d5af90c296b3a0d0d98b2dcf23a21f48dc54c87b
= <babic.milan999@yahoo.com>
2018-01-21 20:44:04 +0100

    HTTPS configuration

---
+++ src/main/java/dmz/insurance/SSLConfig.java
@@ -0,0 +1,39 @@
+package dmz.insurance;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+@Configuration
+public class SSLConfig {
+
+    @Bean
+    public RestTemplate getRestTemplate() throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException {
+        TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+            @Override
+            public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                return true;
+            }
+        };
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
+    }
+
+}
--- src/main/java/dmz/insurance/rest/MainController.java
+++ src/main/java/dmz/insurance/rest/MainController.java
@@ -1,15 +1,23 @@
 package dmz.insurance.rest;
 
 import javax.annotation.PostConstruct;
+import javax.net.ssl.SSLContext;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +30,12 @@ import org.springframework.web.client.RestTemplate;
 
 import dmz.insurance.DTO.OsiguranjeDTO;
 import dmz.insurance.bean.BuyPolicyDTO;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 @RestController
 @CrossOrigin
@@ -36,61 +50,55 @@ public class MainController {
 	
 	@Value("${error.origin.name}")
 	private String errorOriginName; 
-	
-	private RestTemplate rt = new RestTemplate();
+
+	@Autowired
+	private RestTemplate rt;
 	
 	@PostConstruct
-	public void init() {
-		logger.info("TEST!!!");
+	public void init(){
 	}
 
 	private final Log logger = LogFactory.getLog(this.getClass());
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/dobaviStarosneGrupe")
 	public ResponseEntity<?> dobaviStarosneGrupe() {
-		ResponseEntity<?> response = rt.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviStarosneGrupe", String.class);
+		ResponseEntity<?> response = rt.getForEntity("https://" + this.corpUrl + "/corpMain/dobaviStarosneGrupe", String.class);
 		return response;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/dobaviRegione")
 	public ResponseEntity<?> dobaviDestinacije() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<?> response = restTemplate.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviRegione", String.class);
+		ResponseEntity<?> response = rt.getForEntity("https://" + this.corpUrl + "/corpMain/dobaviRegione", String.class);
 		return response;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/dobaviSvrheOsiguranja")
 	public ResponseEntity<?> dobaviSvrheOsiguranja() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<?> response = restTemplate.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviSvrheOsiguranja", String.class);
+		ResponseEntity<?> response = rt.getForEntity("https://" + this.corpUrl + "/corpMain/dobaviSvrheOsiguranja", String.class);
 		return response;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/dobaviPaketeOsiguranja")
 	public ResponseEntity<?> dobaviPaketeOsiguranja() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<?> response = restTemplate.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviPaketeOsiguranja", String.class);
+		ResponseEntity<?> response = rt.getForEntity("https://" + this.corpUrl + "/corpMain/dobaviPaketeOsiguranja", String.class);
 		return response;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/dobaviStarostiStana")
 	public ResponseEntity<?> dobaviStarostiStana() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<?> response = restTemplate.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviStarostiStana", String.class);
+		ResponseEntity<?> response = rt.getForEntity("https://" + this.corpUrl + "/corpMain/dobaviStarostiStana", String.class);
 		return response;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/dobaviProcenjeneVrednostiStana")
 	public ResponseEntity<?> dobaviProcenjeneVrednostiStana() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<?> response = restTemplate.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviProcenjeneVrednostiStana", String.class);
+		ResponseEntity<?> response = rt.getForEntity("https://" + this.corpUrl + "/corpMain/dobaviProcenjeneVrednostiStana", String.class);
 		return response;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/dobaviOsiguranjaStana")
 	public ResponseEntity<?> dobaviOsiguranjaStana() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<?> response = restTemplate.getForEntity("http://" + this.corpUrl + "/corpMain/dobaviOsiguranjaStana", String.class);
+		ResponseEntity<?> response = rt.getForEntity("https://" + this.corpUrl + "/corpMain/dobaviOsiguranjaStana", String.class);
 		return response;
 	}
 
@@ -101,7 +109,7 @@ public class MainController {
 
 		HttpEntity<OsiguranjeDTO> request = new HttpEntity<>(osiguranjeDTO);
 		
-		ResponseEntity<BuyPolicyDTO> response = rt.postForEntity( "http://" + this.paymentDMZUrl +
+		ResponseEntity<BuyPolicyDTO> response = rt.postForEntity( "https://" + this.paymentDMZUrl +
 				"/paymentDMZMain/buyPolicy", request , BuyPolicyDTO.class );
 		
 		System.out.println(response.getBody());
